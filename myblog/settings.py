dce68882919ab09846489e4d5e9b3264f79b1845
Zhouzhehaospace <841606482@qq.com>
2021-09-08 17:25:17 +0800

    更换主机地址

--- myblog/settings.py
+++ myblog/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
     # },
     'default':{
         'ENGINE': 'django.db.backends.mysql',
-        'HOST' : '127.0.0.1',
+        'HOST' : '47.109.84.168',
         'PORT' : '3306',
         'USER' : 'root',
         'PASSWORD' : '19980620',
